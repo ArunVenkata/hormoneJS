0351f114968da9aa8da25f243e2cb67269d9d530
Toship <toship.sonkusare@yellow.ai>
2024-01-13 12:09:53 +0530

    Restructured folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-const middleware = require("./middleware");
+const express = require('express');
+const { registerMiddlewares } = require('./middleware_reader'); 
+const { registerRoutes }  = require("./url_base");
 
-const settings = require("./settings");
+// const {APIS, defaultRoute} = require("./apis.js")
 
-module.exports = {
-    settings,
-    middleware
-}
+( async function(){
+  const app = express();
+
+
+
+  // Read settings.js from filepath present in HARMONY_SETTINGS_FILE env var
+  // Register all apps TODO: Later
+  
+  // Register All Middlewares
+  
+  await registerMiddlewares(app);
+  
+  await registerRoutes(app);
+  
+  
+  // app.use(express.json());
+  // app.use(cors());
+  // app.set("views", __dirname + "/views/");
+  // const DB_URI = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`;
+  // const DB_URI = "";
+  // mongoose.connect(DB_URI);
+  
+  // Add custom Response Object
+  express.response.Response = function (data, status=200) {
+    return this.status(status).json(data);
+  };
+  
+  
+  
+  app.use((req, res, next) => {
+    res.Response = res.Response.bind(res);
+    next();
+  });
+  
+  
+  
+  
+  // Provide error handling for invalid URLs
+  app.use((request, response) => {
+    response.status(404).json({ error: "Invalid API request" });
+  });
+  
+  // listen for requests :)
+  const listener = app.listen(3000, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+  });
+  
+})()
--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ function isInstanceOf(obj, _class){
 
 
 async function dynamicBaseImport(_path="", varName=undefined){
-    let import_string = path.join(process.cwd(), "src", _path);
+    let import_string = path.join(process.cwd(), "../example/", _path);     // TODO: Fix the path, make it more dynamic
     console.log(import_string);
     if(typeof varName === "string"){
         return (await import(import_string))[varName];
